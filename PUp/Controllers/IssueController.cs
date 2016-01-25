a5604bac5e59272b8c72209cce38d94dd4eac582
mohamedb <mohamed.boullouz@gmail.com>
2016-01-25 22:11:21 +0100

    Add a view for issues liste

---
+++ PUp/Controllers/IssueController.cs
@@ -0,0 +1,33 @@
+﻿using PUp.Models;
+using PUp.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PUp.Controllers
+{
+    public class IssueController : Controller
+    {
+        //TODO user a simple factory 
+        private TaskRepository taskRepository;
+        private ProjectRepository projectRepository;
+        private UserRepository userRepository;
+        private ContributionRepository contributionRepository;
+        private DatabaseContext dbContext = new DatabaseContext();
+
+        public IssueController()
+        {
+            userRepository = new UserRepository(dbContext);
+            taskRepository = new TaskRepository(dbContext);
+            projectRepository = new ProjectRepository(dbContext);
+            contributionRepository = new ContributionRepository(dbContext);
+        }
+        // GET: liste of  Issues by project id
+        public ActionResult Index(int id)
+        {             
+            return View(projectRepository.FindById(id));
+        }
+    }
+}
