3435eccc547299710f879c542cc5801ccdd9ce01
mohamedb <mohamed.boullouz@gmail.com>
2016-01-31 19:29:14 +0100

    Show issues in the timeline

--- PUp/ViewModels/Project/ProjectTimelineViewModel.cs
+++ PUp/ViewModels/Project/ProjectTimelineViewModel.cs
@@ -14,23 +14,26 @@ namespace PUp.ViewModels.Project
          
         public List<object> Elements { get; set; }
         public List<NotificationEntity> Notifs { get; set; }
+        public List<IssueEntity> Issues { get; set; }
         private List<object> AllElements = new List<object>();
         public ProjectTimelineViewModel(ProjectEntity p)
         {
             Elements = new List<object>();
             Project = p;
             Notifs = new List<NotificationEntity>();
+            Issues = p.Issues.ToList();
             NotificationRepository notifRepository = new NotificationRepository();
-            Notifs = notifRepository.GetAll();
-           Tasks = p.Tasks.ToList();
+            Notifs = notifRepository.GetAll();//Todo Remove this
+            Tasks = p.Tasks.ToList();
             Init();
         }
         public void Init()
         {
             Tasks.ForEach(t => AllElements.Add(t));
             Notifs.ForEach(n => AllElements.Add(n));
+            Issues.ForEach(i => AllElements.Add(i));
             AllElements  =AllElements.OrderByDescending(ob => ((IBasicEntity)ob).AddAt).ToList() ;
-             
+
             AllElements.ForEach(obj => Elements.Add(obj));
         }
     }
