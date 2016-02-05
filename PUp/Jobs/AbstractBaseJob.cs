1894fd8807e55b54667196f67f037c5baa87c440
MohamedB <mohamedb@users.noreply.github.com>
2016-02-05 13:11:30 +0100

    Create a base class for jobs

---
+++ PUp/Jobs/AbstractBaseJob.cs
@@ -0,0 +1,29 @@
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PUp.Jobs
+{   
+        /// <summary>
+        /// TODO: complete this..
+        ///  Set a base for jobs
+        /// </summary>
+        /// <param name="context"></param>
+    public abstract class AbstractBaseJob : IJob
+    {
+      protected ProjectRepository projectRepo;
+      protected NotificationRepository notificationRepo;
+      protected UserRepository userRepo;
+      protected DatabaseContext dbContext = new DatabaseContext();
+      
+      public virtual void Init(){
+        projectRepo = new ProjectRepository(dbContext);
+        notificationRepo = new NotificationRepository(dbContext);
+        userRepo = new UserRepository(dbContext);
+      }
+    
+    }
+    
+}
