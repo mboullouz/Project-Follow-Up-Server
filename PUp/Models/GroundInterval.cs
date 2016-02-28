f8a9caba4cba5710312d860e7aa17168039cf0e8
mohamedb <mohamed.boullouz@gmail.com>
2016-02-28 19:19:37 +0100

    Start counting avelaible hour from current hour

--- PUp/Models/GroundInterval.cs
+++ PUp/Models/GroundInterval.cs
@@ -17,40 +17,52 @@ namespace PUp.Models
 
         public void Init()
         {
-            for(var hourKey= AppConst.DayStart; hourKey <= AppConst.DayEnd; hourKey++)
+            for (var hourKey = AppConst.DayStart; hourKey <= AppConst.DayEnd; hourKey++)
             {
-               Interval.Add(new KeyValuePair<int, bool>(hourKey, false));
+                Interval.Add(new KeyValuePair<int, bool>(hourKey, false));
+            }
+        }
+
+        public void MakeNotAvelaibleHoursBeforeNow()
+        {
+            int actualHour = DateTime.Now.Hour;
+            for (var hourKey = AppConst.DayStart; hourKey <= actualHour; hourKey++)
+            {
+                Interval[hourKey] = true;
             }
         }
 
         //TODO: Prevent add when current datetime.hour>AppConst.DayEnd
-        public IDictionary<int, bool> AddDate(DateTime startDate,int durationInHours)
-        {   
+        //TODO USE CheckForDate before start adding!
+        public IDictionary<int, bool> AddDate(DateTime startDate, int durationInHours)
+        {
             int startH = startDate.Hour;
-            
-            if(startH < AppConst.DayStart || startH > AppConst.DayEnd)
+            MakeNotAvelaibleHoursBeforeNow();
+            if (startH < AppConst.DayStart || startH > AppConst.DayEnd)
             {
                 return Interval;
-            } 
-            for (var h = startH; h < startH+durationInHours; h++)
-            {
-                Interval[h] = true;
             }
-            return Interval; 
+            for (var h = startH; h < startH + durationInHours; h++)
+            {
+                if(!Interval[h])
+                    Interval[h] = true;
+            }
+            return Interval;
         }
 
-        public bool CheckForDateAndDuration(DateTime startDate,int duration)
+        public bool CheckForDateAndDuration(DateTime startDate, int duration)
         {
+            MakeNotAvelaibleHoursBeforeNow();
             if (IsFull())
             {
                 return false;
             }
             int startH = startDate.Hour;
-            if (Interval[startH] || startH+duration>AppConst.DayEnd | startH<DateTime.Now.Hour)
+            if (Interval[startH] || startH + duration > AppConst.DayEnd | startH < DateTime.Now.Hour)
             {
                 return false;
             }
-            for(int i = startH; i < startH + duration; i++)
+            for (int i = startH; i < startH + duration; i++)
             {
                 if (Interval[i])
                 {
