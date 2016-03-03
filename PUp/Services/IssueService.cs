b8693dc7bcc2efe7b49606f71d12b0d43d889bd4
mohamedb <mohamed.boullouz@gmail.com>
2016-03-04 00:09:53 +0100

    Add a service class  for issues

---
+++ PUp/Services/IssueService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PUp.Services
+{
+    public class IssueService:BaseService
+    { 
+        public IssueService(ModelStateWrapper modelStateWrapper) : base(modelStateWrapper) { }
+    }
+}
