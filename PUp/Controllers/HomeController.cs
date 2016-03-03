58b7b3f74a0b96212ccf9e07162718ae8644ae12
mohamedb <mohamed.boullouz@gmail.com>
2016-03-03 22:44:07 +0100

    Add a project service and refactore home controller

--- PUp/Controllers/HomeController.cs
+++ PUp/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using PUp.Models;
 using PUp.Models.Entity;
 using PUp.Models.Repository;
+using PUp.Services;
 using PUp.ViewModels.Project;
 using System;
 using System.Collections.Generic;
@@ -15,33 +16,23 @@ namespace PUp.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-
+        private ProjectService projectService;
         private RepositoryManager repo = new RepositoryManager();
         private UserEntity currentUser = null;
 
         public HomeController()
         {
-            currentUser = repo.UserRepository.GetCurrentUser();
+            projectService = new ProjectService(new ModelStateWrapper(TempData, ModelState));
         }
         public ActionResult Index()
         {
-            if(currentUser==null)
+            currentUser = repo.UserRepository.GetCurrentUser();
+            if (currentUser==null)
             {
                 this.Flash("Please register / or login if you already have an account", FlashLevel.Warning);
                 return RedirectToAction("Register", "Account");
             }
-            var projectsByUser = repo.ProjectRepository.GetAll()
-                                     .Where(p=>p.Owner==currentUser|| p.Contributors.Contains(currentUser))
-                                     .OrderByDescending(p=>p.StartAt).ToList();
-            TableProjectModelView tableProject = new TableProjectModelView
-            {
-                CurrentUser = currentUser,
-                Projects = projectsByUser,
-                OtherProjects = repo.ProjectRepository.GetActive()
-                                    .Where(p => !projectsByUser.Contains(p))
-                                    .OrderByDescending(p => p.StartAt).ToList(),
-            };
-            return View(tableProject);
+            return View(projectService.GetTableProjectForCurrentUser());
         }
 
         public ActionResult About()
---
+++ PUp/Services/ProjectService.cs
@@ -0,0 +1,29 @@
+﻿using PUp.ViewModels.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PUp.Services
+{   
+    public class ProjectService: BaseService
+    {
+        public ProjectService(ModelStateWrapper modelStateWrapper) : base(modelStateWrapper) { }
+
+        public TableProjectModelView GetTableProjectForCurrentUser()
+        {
+            var projectsByUser = repo.ProjectRepository.GetAll()
+                                     .Where(p => p.Owner == currentUser || p.Contributors.Contains(currentUser))
+                                     .OrderByDescending(p => p.StartAt).ToList();
+            TableProjectModelView tableProject = new TableProjectModelView
+            {
+                CurrentUser = currentUser,
+                Projects = projectsByUser,
+                OtherProjects = repo.ProjectRepository.GetActive()
+                                    .Where(p => !projectsByUser.Contains(p))
+                                    .OrderByDescending(p => p.StartAt).ToList(),
+            };
+            return  tableProject ;
+        }
+    }
+}
