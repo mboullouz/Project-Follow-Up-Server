1c06a3ec01676b1d177e20c4b045c13eafbcb72a
mohamedb <mohamed.boullouz@gmail.com>
2016-03-06 20:24:06 +0100

    UI: add projects to the sidebare

---
+++ PUp/ViewModels/MenuModelView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PUp.ViewModels
+{
+    public class MenuModelView
+    {
+        public static List<Models.Entity.ProjectEntity> ActiveProject()
+        {
+            Models.DatabaseContext dbc = new Models.DatabaseContext();
+            return dbc.ProjectSet.Where(p => p.EndAt > DateTime.Now && p.Deleted == false).ToList();
+        }
+    }
+}
