e8131716066ce7329c058273f1f6515b8e7093c8
mohamedb <mohamed.boullouz@gmail.com>
2016-03-25 01:01:04 +0100

    Compute time ago and make it available for UI

--- PUp/Models/SimpleObject/NotificationDto.cs
+++ PUp/Models/SimpleObject/NotificationDto.cs
@@ -20,25 +20,53 @@ namespace PUp.Models.SimpleObject
         public DateTime? DeleteAt { get; set; }
         public bool? Deleted { get; set; }
 
+
         public NotificationDto(NotificationEntity notif, int depth = AppConst.MaxDepth)
         {
             Init(notif, 1);
         }
 
-        public void Init(NotificationEntity notif,int depth=AppConst.MaxDepth)
+        public void Init(NotificationEntity notif, int depth = AppConst.MaxDepth)
         {
-            Id = notif.Id ;
-            Url = notif.Url ;
-            Message = notif.Message ;
-            Seen = notif.Seen ;
-            Level = notif.Level ;
-            User = new UserDto(notif.User,depth) ;
-            AddAt = notif.AddAt ;
-            EditAt = notif.EditAt ;
-            DeleteAt = notif.DeleteAt ;
-            Deleted = notif.Deleted ;
+            Id = notif.Id;
+            Url = notif.Url;
+            Message = notif.Message;
+            Seen = notif.Seen;
+            Level = notif.Level;
+            User = new UserDto(notif.User, depth);
+            AddAt = notif.AddAt;
+            EditAt = notif.EditAt;
+            DeleteAt = notif.DeleteAt;
+            Deleted = notif.Deleted;
+
+            TimeAgo = ComputeTimeAgo(notif.AddAt);
+        }
+
+        //Additional 
+        public string TimeAgo { get; set; }
+
+        public string ComputeTimeAgo(DateTime dateFrom)
+        {
+            var diff = DateTime.Now - dateFrom;
+            if (diff.TotalMinutes < 1)
+            {
+                return ((int)diff.TotalSeconds) + " s ago";
+            }
+            else if (diff.TotalHours < 1)
+            {
+                return ((int)diff.TotalMinutes) + "min ago";
+            }
+            else if (diff.TotalDays < 1)
+            {
+                return dateFrom.ToString("HH\\Hmm");
+            }
+            else
+            {
+                return AddAt.ToString("d-MM-yyyy HH\\Hmm");
+            }
+
         }
     }
 
-    
+
 }
