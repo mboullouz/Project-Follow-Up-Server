6e4dcd4c8e1ff3a4f03c26068f66a94abf31ce85
mohamedb <mohamed.boullouz@gmail.com>
2016-04-03 18:02:02 +0200

    Api registration

--- PUp/Controllers/AccountApiController.cs
+++ PUp/Controllers/AccountApiController.cs
@@ -32,16 +32,17 @@ namespace PUp.Controllers
         {
 
             var UserManager = new UserManager<UserEntity>(new UserStore<UserEntity>(new DatabaseContext()));
-            UserEntity user=null;
-            try {//model may = or contain null
-               user= UserManager.Find(model.Username, model.Password);
+            UserEntity user = null;
+            try
+            {//model may = or contain null
+                user = UserManager.Find(model.Username, model.Password);
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return Content(HttpStatusCode.Forbidden, "0");
             }
-             
-            return Content(HttpStatusCode.OK, user==null?"0":"1");
+
+            return Content(HttpStatusCode.OK, user == null ? "0" : "1");
         }
 
 
@@ -56,6 +57,55 @@ namespace PUp.Controllers
         public NegotiatedContentResult<string> Verify()
         {
             return Content(HttpStatusCode.OK, "1");
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [System.Web.Mvc.ValidateAntiForgeryToken]
+        public async System.Threading.Tasks.Task<NegotiatedContentResult<string>> Register(ViewModels.Auth.RegisterViewModel model)
+        {
+            ValidationMessageHolder validationMessageHolder = new ValidationMessageHolder();
+            if (ModelState.IsValid)
+            {
+                var user = new UserEntity { Name = model.Name, UserName = model.Email, Email = model.Email };
+                var UserManager = new UserManager<UserEntity>(new UserStore<UserEntity>(new DatabaseContext()));
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // Pour plus d'informations sur l'activation de la confirmation du compte et la réinitialisation du mot de passe, consultez http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Envoyer un message électronique avec ce lien
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirmez votre compte", "Confirmez votre compte en cliquant <a href=\"" + callbackUrl + "\">ici</a>");
+
+                    return Content(HttpStatusCode.OK, validationMessageHolder.ToJson());
+                }
+
+                var errorCounter = 0;
+                foreach (var e in result.Errors)
+                {
+                    validationMessageHolder.Add("ModelState:" + errorCounter++, e);
+                }
+
+            }
+            else
+            {
+                var errorCounter = 0;
+                foreach (var v in ModelState.Values)
+                {
+                    foreach (var e in v.Errors)
+                    {
+                        validationMessageHolder.Add("ModelState:" + errorCounter++, e.ErrorMessage);
+                    }
+                        
+                }
+            }
+
+            // Si nous sommes arrivés là, un échec s’est produit. Réafficher le formulaire
+            return Content(HttpStatusCode.NotAcceptable, validationMessageHolder.ToJson());
         }
 
     }
---
+++ PUp/ViewModels/Auth/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PUp.ViewModels.Auth
+{
+    public class RegisterViewModel:BaseModelView
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "String {0} must be at last {2} caracters.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "String {0} must be at last {2} caracters.", MinimumLength = 6)]
+        [DataType(DataType.Text)]
+        [Display(Name = "Full Name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password ")]
+        [Compare("Password", ErrorMessage = "Password not matching.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
