e8318812b618b84fc91e5f30381d8bdd8b8258e6
mohamedb <mohamed.boullouz@gmail.com>
2016-04-07 23:23:26 +0200

    Extract Simple K,V object to a separate class

---
+++ PUp/ViewModels/SimpleKeyValue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PUp.ViewModels
+{
+
+
+    /// <summary>
+    ///  Simple Key Value Class to use as object for list instead of Dictionnary 
+    ///    this is a simplification for UI that have hard time iterating over lists like
+    ///    { {a:b},{a,c } } 
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public class SimpleKeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+
+    }
+}
