09c77f0582cf046670c99d2c2b2278a561f910cd
mohamedb <mohamed.boullouz@gmail.com>
2016-04-08 14:15:56 +0200

    Add annotation to the view model

--- PUp/ViewModels/AddIssueViewModel.cs
+++ PUp/ViewModels/AddIssueViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using PUp.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +9,19 @@ using System.Web.Mvc;
 namespace PUp.ViewModels
 {
     public class AddIssueViewModel
-    {
+    {   
+
+        /// <summary>
+        /// Required and iniialized to 0 in case of new 
+        /// </summary>
+        [Required]
         public int Id { get; set; }
+        [Required]
         public int ProjectId { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be between {1} and {2} caracters.", MinimumLength = 15)]
         public string Description { get; set; }
+        [Required]
         public string Status { get; set; }
         public List<SimpleKeyValue<string,string>> StatusList { get; set; }
         public bool Deleted { get; set; }
@@ -29,10 +39,12 @@ namespace PUp.ViewModels
         }
         public void Init()
         {
+            Id = 0;
             StatusList.Add(new SimpleKeyValue<string, string> { Key = "false", Value = IssueStatus.Open });
             StatusList.Add(new SimpleKeyValue<string, string> { Key = "true", Value = IssueStatus.Resolved });
             Deleted = false;
             CreateAt = DateTime.Now;
+            Status = StatusList.FirstOrDefault().Key; //init 
         }
     }
 }
