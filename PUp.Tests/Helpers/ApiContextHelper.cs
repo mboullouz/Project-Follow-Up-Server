7c81c890121e229440d074628954beeebee318d7
mohamedb <mohamed.boullouz@gmail.com>
2016-04-25 21:12:46 +0200

    Refact' Api tests

--- PUp.Tests/Helpers/ApiContextHelper.cs
+++ PUp.Tests/Helpers/ApiContextHelper.cs
@@ -1,14 +1,48 @@
-﻿using PUp.Controllers;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PUp.Controllers;
+using PUp.Models;
+using PUp.Models.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace PUp.Tests.Helpers
 {
     class ApiContextHelper
-    {
-       
+    {  
+        public static UserEntity CurrentUser
+        {
+            get
+            {
+                return GetContextCurrentUser();
+            }
+        }
+       public static void MockApiControllerRequest(ApiController apiController)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new DatabaseContext()));
+            string[] roles = new string[ CurrentUser.Roles.ToList().Count];
+            var k = 0;
+            foreach (var r in  CurrentUser.Roles.ToList())
+            {
+                roles[k] = r.RoleId; k++;
+            }
+            apiController.RequestContext.Principal = new GenericPrincipal(new GenericIdentity(CurrentUser.Email, "Basic"), roles);
+            apiController.Request = new HttpRequestMessage();
+        }
+
+        public static UserEntity GetContextCurrentUser()
+        {
+            string userName = "m@boullouz.com";
+            string password = "123*Aa";
+            var UserManager = new UserManager<UserEntity>(new UserStore<UserEntity>(new DatabaseContext()));
+            var user = UserManager.Find(userName, password);
+            return user;
+        }
     }
 }
--- PUp.Tests/TaskTest/TaskApiTest.cs
+++ PUp.Tests/TaskTest/TaskApiTest.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Security.Principal;
 using System.Net.Http;
 using PUp.Models.Dto;
+using PUp.Tests.Helpers;
 
 namespace PUp.Tests.TaskTest
 {
@@ -20,25 +21,11 @@ namespace PUp.Tests.TaskTest
         public void Access_api_task_controller()
         {
             TaskApiController apiController = new TaskApiController();
-            string userName = "m@boullouz.com";
-            string password = "123*Aa";
-            var UserManager = new UserManager<UserEntity>(new UserStore<UserEntity>(new DatabaseContext()));
-            var user = UserManager.Find(userName, password);
-
-            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new DatabaseContext()));
-            string[] roles = new string[user.Roles.ToList().Count];
-            var k = 0;
-            foreach (var r in user.Roles.ToList())
-            {
-                roles[k] = r.RoleId; k++;
-            }
-            apiController.RequestContext.Principal = new GenericPrincipal(new GenericIdentity(userName, "Basic"), roles);
-            apiController.Request= new HttpRequestMessage();
-           // apiController.Request.Content = new HttpContent();
+            ApiContextHelper.MockApiControllerRequest(apiController);
             var taskDtoResponse = apiController.Get(1).Content;
             var taskOne = rep.TaskRepository.FindById(1);
             var taskOneDto = new TaskDto(taskOne);
-            Assert.IsNotNull(user);
+            Assert.IsNotNull(ApiContextHelper.CurrentUser);
             Assert.IsNotNull(taskDtoResponse);
             Assert.IsNotNull(taskOne);
             Assert.AreEqual(taskOne.Id,1);
